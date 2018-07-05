31ce85c894e46bd4e1abe08bf806250524e7fd44
pervez <pervez.cse@gmail.com>
2018-07-05 15:55:15 +0600

    Increase readability

--- src/test/java/interview/unittest/StatisticsServiceTest.java
+++ src/test/java/interview/unittest/StatisticsServiceTest.java
@@ -1,14 +1,15 @@
 package interview.unittest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import interview.transaction.entity.Statistics;
-import interview.transaction.entity.Statistics.StatisticsBuilder;
 import interview.transaction.entity.Transaction;
 import interview.transaction.service.StatisticsService;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 import javax.annotation.Resource;
 import org.junit.Test;
@@ -39,11 +40,10 @@ public class StatisticsServiceTest {
 
   @Test
   public void givenMultipleTransactionInLast60Seconds_whenGetStatistics_thenReturnStatistics() {
-    StatisticsBuilder statisticsBuilder = Statistics
-        .builder(new Transaction(0.0, System.currentTimeMillis()));
-    LongStream.range(1, 10).forEach(
-        a -> statisticsBuilder.transaction(new Transaction((double)a, System.currentTimeMillis())));
-    Statistics stat = statisticsBuilder.build();
+    List<Transaction> txList = LongStream.range(0, 10)
+        .mapToObj(a -> new Transaction((double) a, System.currentTimeMillis()))
+        .collect(Collectors.toList());
+    Statistics stat = Statistics.builder(txList).build();
     when(statsRepository.get(any(Long.class))).thenReturn(stat);
     Statistics result = statisticsService.getStatistics();
     assertThat(result.getSum()).isEqualTo(stat.getSum());
